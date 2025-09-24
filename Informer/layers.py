068ac4ebfdf134a9dc53cf479f19f9b6a1bbd1b7
amir <dmohmmadamir@gmail.com>
2025-09-25 00:01:24 +0330

    input generated

--- Informer/layers.py
+++ Informer/layers.py
@@ -11,7 +11,7 @@ class Distilling(layers.Layer):
                                        activation='elu')
         
         self.polling_layer= layers.MaxPool1D(pool_size=2,   
-                                        strides= 2)
+                                             strides= 2)
     
     def call(self, inputs):
         x= self.conv_layer(inputs)
@@ -19,13 +19,39 @@ class Distilling(layers.Layer):
         
         return x
     
+class embedding(layers.Layer):
+    def __init__(self,
+                 d_model= 512,
+                 dropout= 0.1,
+                 **kwargs):
+        self.value_embedding= layers.Dense(d_model)
+        self.positional_embedding= self.add_weight(
+            "pos_emb", shape=(1, 1000, d_model), initializer='random_normal'
+            )
+        self.dropout= layers.Dropout(dropout)
+        
+    def call(self, inputs):
+        value_emb= self.value_embedding(inputs)
+        seq_len= tf.shape(inputs)[1]
+        x= value_emb + self.positional_embedding[:, :seq_len, :]
+        return self.dropout(x)
+    
 class ProbSparce(layers.Layer):
     def __init__(self, 
+                 d_model,
                  **kwargs):
-        pass
+        self.input_conv= layers.Conv1D(kernel_size= 3, 
+                                       filters=d_model,
+                                       padding='same', 
+                                       activation='elu')
+        self.input_emb= embedding()
     
-    def build(self, input_shape):
-        pass
+    def build(self, inputs):
+        # generate input 
+        input_conv= self.input_conv(inputs)
+        input_emb= self.input_emb(inputs)
+        input_val= input_conv + input_emb 
+        
     
     def call(self, inputs):
         pass
